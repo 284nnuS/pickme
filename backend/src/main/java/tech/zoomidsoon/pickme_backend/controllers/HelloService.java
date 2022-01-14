be44878497805cb462871ae81d0a01df5054bb6f
nez21 <chuongvbvde150302@fpt.edu.vn>
2022-01-14 14:50:55 +0700

    Project structure reconfiguration

--- backend/src/main/java/tech/zoomidsoon/pickme_backend/controllers/HelloService.java
+++ backend/src/main/java/tech/zoomidsoon/pickme_backend/controllers/HelloService.java
@@ -1,11 +1,12 @@
-package tech.zoomidsoon.pickme_backend;
+package tech.zoomidsoon.pickme_backend.controllers;
 
 import javax.ws.rs.Path;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.MediaType;
+import tech.zoomidsoon.pickme_backend.models.Hello;
 
 @Path("/hello")
 public class HelloService {
