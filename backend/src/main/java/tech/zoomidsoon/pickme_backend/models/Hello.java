1be7a933c7f3e2a60ff62102376ea0d4c39ba962
nez21 <chuongvbvde150302@fpt.edu.vn>
2022-01-19 11:48:52 +0700

    Add Lombok

--- backend/src/main/java/tech/zoomidsoon/pickme_backend/models/Hello.java
+++ backend/src/main/java/tech/zoomidsoon/pickme_backend/models/Hello.java
@@ -1,20 +1,18 @@
 package tech.zoomidsoon.pickme_backend.models;
 
+import javax.persistence.Entity;
+
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Hello {
    private String message;
 
-   public Hello() {
-   }
-
    public Hello(String message) {
-      this.message = message;
-   }
-
-   public String getMessage() {
-      return this.message;
-   }
-
-   public void setMessage(String message) {
       this.message = message;
    }
 }
