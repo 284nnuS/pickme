7ec9d6754e631cf83d1bb4a3f58cc13e01ab5708
Nez21 <chuongvbvde150302@fpt.edu.vn>
2022-02-06 22:04:48 +0700

    User CRUD example

--- java/src/main/java/tech/zoomidsoon/pickme_restful_api/models/User.java
+++ java/src/main/java/tech/zoomidsoon/pickme_restful_api/models/User.java
@@ -1,5 +1,19 @@
 package tech.zoomidsoon.pickme_restful_api.models;
 
+import javax.persistence.Entity;
+
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class User {
+	private String username;
+
+	public User(String username) {
+		this.username = username;
+	}
 
 }
--- java/src/main/java/tech/zoomidsoon/pickme_restful_api/repos/UserRepository.java
+++ java/src/main/java/tech/zoomidsoon/pickme_restful_api/repos/UserRepository.java
@@ -1,12 +1,16 @@
 package tech.zoomidsoon.pickme_restful_api.repos;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import tech.zoomidsoon.pickme_restful_api.mappers.UserRowMapper;
 import tech.zoomidsoon.pickme_restful_api.models.User;
+import tech.zoomidsoon.pickme_restful_api.utils.DBContext;
 
-public class UserRepository implements Repository<User>{
+public class UserRepository implements Repository<User> {
 
 	@Override
 	public User create(User entity) {
@@ -16,7 +20,14 @@ public class UserRepository implements Repository<User>{
 
 	@Override
 	public List<User> read(Criteria criteria) {
-		// TODO Auto-generated method stub
+		try {
+			try (Connection connection = DBContext.getConnection()) {
+				ResultSet result = criteria.query(connection);
+				UserRowMapper urm = new UserRowMapper();
+				return urm.processResultSet(result, User.class);
+			}
+		} catch (Exception e) {
+		}
 		return null;
 	}
 
@@ -38,14 +49,19 @@ public class UserRepository implements Repository<User>{
 		return null;
 	}
 
-	public static class FindById implements Criteria{
-        
-		
+	public static class FindById implements Criteria {
+
 		@Override
 		public ResultSet query(Connection conn) {
-			// TODO Auto-generated method stub
+			try {
+				try (PreparedStatement stmt = conn.prepareStatement("select * from tbluser")) {
+					ResultSet rs = stmt.executeQuery();
+					return rs;
+				}
+			} catch (SQLException e) {
+			}
 			return null;
 		}
-		
+
 	}
 }
