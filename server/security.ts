6ae422c9eba0dfcbe9704b0e5db7c93a1b169306
Nez21 <chuongvbvde150302@fpt.edu.vn>
2022-02-08 19:30:43 +0700

    Add userRole in session data

--- server/security.ts
+++ server/security.ts
@@ -1,12 +1,54 @@
-/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from 'express'
-import { cache } from '../shared/cache'
-import { getToken } from 'next-auth/jwt'
+import { getToken, JWT } from 'next-auth/jwt'
 import { NextApiRequest, NextApiResponse } from 'next'
-import NextAuth from 'next-auth'
+import NextAuth, { getServerSession } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+import { Account, Profile, Session, User } from 'next-auth/core/types'
 
 const baseUrl = '/api/auth/'
+
+const nextAuthOptions = {
+   providers: [
+      GoogleProvider({
+         clientId: process.env.GOOGLE_CLIENT_ID,
+         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      }),
+   ],
+   pages: {
+      signIn: '/auth/signIn',
+      signOut: '/auth/signOut',
+      error: '/auth/error',
+   },
+   callbacks: {
+      async signIn({
+         user,
+         account,
+         profile,
+         email,
+         credentials,
+      }: {
+         user: User
+         account: Account
+         profile: Profile
+         email
+         credentials
+      }) {
+         return true
+      },
+      async redirect({ url, baseUrl }) {
+         if (url.startsWith(baseUrl)) return url
+         else if (url.startsWith('/')) return new URL(url, baseUrl).toString()
+         return baseUrl
+      },
+      async session({ session, user, token }: { session: Session; user: User; token: JWT }) {
+         session.user.role = 'user'
+
+         return session
+      },
+   },
+   secret: process.env.SECRET_KEY,
+}
 
 export async function nextAuth(req: Request, res: Response, next: NextFunction) {
    if (!req.url.startsWith(baseUrl)) {
@@ -15,33 +57,7 @@ export async function nextAuth(req: Request, res: Response, next: NextFunction) 
 
    req.query.nextauth = req.url.slice(baseUrl.length).replace(/\?.*/, '').split('/')
 
-   NextAuth(req as unknown as NextApiRequest, res as unknown as NextApiResponse, {
-      providers: [
-         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-         }),
-      ],
-      pages: {
-         signIn: '/auth/signIn',
-         signOut: '/auth/signOut',
-         error: '/auth/error',
-      },
-      callbacks: {
-         async signIn({ user, account, profile, email, credentials }) {
-            return true
-         },
-         async redirect({ url, baseUrl }) {
-            if (url.startsWith(baseUrl)) return url
-            else if (url.startsWith('/')) return new URL(url, baseUrl).toString()
-            return baseUrl
-         },
-         async session({ session, user, token }) {
-            return session
-         },
-      },
-      secret: process.env.SECRET_KEY,
-   })
+   NextAuth(req as unknown as NextApiRequest, res as unknown as NextApiResponse, nextAuthOptions)
 }
 
 export async function authorize(req: Request, res: Response, next: NextFunction) {
@@ -49,13 +65,13 @@ export async function authorize(req: Request, res: Response, next: NextFunction)
 
    if (!token) return next()
 
-   //Get User Role
-   const getRole = (email: string): string => {
-      return 'user'
-   }
+   const session = await getServerSession(
+      { req: req as unknown as NextApiRequest, res: res as unknown as NextApiResponse },
+      nextAuthOptions,
+   )
 
    res.locals.token = token
-   res.locals.userRole = cache(getRole, token.email)
+   res.locals.userRole = session.user.role
 
    next()
 }
