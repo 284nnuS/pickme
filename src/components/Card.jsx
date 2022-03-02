925de0ba0876dab95f84215f93fd20c5e74e4813
284nnuS <vanson2804@gmail.com>
2022-03-02 21:28:54 +0700

    age of user in Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,12 +7,15 @@ function Card({ data }) {
             style={{
                backgroundImage: `url(https://scontent.fdad3-5.fna.fbcdn.net/v/t1.6435-9/146994747_2854728808188423_7615033960248435066_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=gtQZRbkkXwEAX-XNyGy&_nc_ht=scontent.fdad3-5.fna&oh=00_AT8N16aeFbtlFQ2wMA86wlHCqxM779f34tV0YBFpSUVwQQ&oe=62432D08)`,
             }}
-            className="relative w-[410px] h-[720px] rounded-2xl bg-cover bg-center flex flex-col justify-between"
+            className="relative w-[410px] h-[720px] rounded-2xl bg-cover bg-center flex flex-col justify-between "
          >
             <Player url="/a.mp3" />
 
-            <div className="absolute bottom-0 text-left">
-               <h3 className=" pl-2 font-sans text-white font-black text-4xl bottom-[20vh]  ">{data.name}</h3>
+            <div className="absolute bottom-0 text-left ">
+               <div className="flex items-end">
+                  <h3 className=" pl-2 font-sans text-white font-black text-4xl   ">{data.name}</h3>
+                  <h5 className=" pl-3 font-sans text-white  text-2xl   ">{data.age}</h5>
+               </div>
                <h5 className=" pl-2 font-sans text-white text-xl mb-[15vh]  "> {data.bio} </h5>
             </div>
             <div className="bottom-0 absolute bg-gradient-to-t from-black   h-1/3 w-full"></div>
--- src/components/PickmeCard.jsx
+++ src/components/PickmeCard.jsx
@@ -91,8 +91,8 @@ const PickmeCard = () => {
             <SwipeButton
                handleCloseBtn={() => swipe('left')}
                handleHeartBtn={() => swipe('right')}
-               handleRepeatBtn={() => swipe('right')}
-               handleStarBtn={() => swipe('right')}
+               handleRepeatBtn={() => swipe('up')}
+               handleStarBtn={() => swipe('up')}
             />
          </div>
       </center>
