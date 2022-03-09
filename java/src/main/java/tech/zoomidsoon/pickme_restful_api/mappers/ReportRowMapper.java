68f4c7c82f01b77d2ec0af4999fc96172d734d3f
ThuHoai-Yin <thuhoainvc2001@gmail.com>
2022-03-09 15:33:43 +0700

    ## Add report model
    ## Add report repository
    ## Add report row mapper

---
+++ java/src/main/java/tech/zoomidsoon/pickme_restful_api/mappers/ReportRowMapper.java
@@ -0,0 +1,32 @@
+package tech.zoomidsoon.pickme_restful_api.mappers;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+import tech.zoomidsoon.pickme_restful_api.models.Report;
+
+public class ReportRowMapper extends RowMapper<Report> {
+    private static final RowMapper<Report> singleton = new ReportRowMapper();
+
+    private ReportRowMapper() {
+    }
+
+    public static RowMapper<Report> getInstance() {
+        return singleton;
+    }
+
+    @Override
+    public Boolean mapRow(ResultSet rs, Report obj) throws SQLException {
+        Long reportId = rs.getLong("reportId");
+        Timestamp time = rs.getTimestamp("time");
+        obj.setReportId(reportId);
+        obj.setTime(time != null ? time.getTime() : null);
+        obj.setReporter(rs.getInt("reporter"));
+        obj.setReported(rs.getInt("reported"));
+        obj.setMessage(rs.getString("message"));
+        obj.setDone(rs.getBoolean("done"));
+        return true;
+    }
+
+}
