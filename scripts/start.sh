22ab8095ee37208640d2a75623efa479660b675c
Nez21 <chuong.nzxxi@gmail.com>
2022-03-24 12:23:32 +0700

    Edit scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 export PATH="$PATH:/home/nez21/.local/bin/"
+sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
 cd /home/nez21/pickme && yarn self:start
